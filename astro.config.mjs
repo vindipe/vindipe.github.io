369f5cf5b04a87b07109942d16b003c536c71b7b
vincenzo-dip8 <vincenzopaolo.diperna@outlook.com>
2026-05-06 18:30:20 +0200

    Remove unused blog store and services template code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
+// import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vindipe.github.io",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind()],
 });
